569d3ae848e2d26337113980ab60bc0abe19171f
vrbaj <vrbik.vrbik@gmail.com>
2018-09-12 15:38:42 +0200

    part 1, example 4

---
+++ part1ex004.py
@@ -0,0 +1,16 @@
+"""
+Write a Python program which accepts the radius of a circle from the user and compute the area. Go to the editor
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504
+"""
+import math
+
+PI = math.pi
+radius = input()
+try:
+    radius = float(radius)
+    print("r = ", radius)
+    print("Area = ", PI * radius ** 2)
+except Exception as ex:
+    print("Exception occured", ex)
